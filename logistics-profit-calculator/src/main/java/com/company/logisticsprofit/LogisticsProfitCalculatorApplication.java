14335f466917b8ca5c012d7071bdd5bfc9289a22
Pedro <dinizpedro01@gmail.com>
2025-02-28 15:02:07 +0000

    initial commit

---
+++ logistics-profit-calculator/src/main/java/com/company/logisticsprofit/LogisticsProfitCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.company.logisticsprofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsProfitCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsProfitCalculatorApplication.class, args);
+	}
+
+}
---
+++ logistics-profit-calculator/src/test/java/com/company/logisticsprofit/LogisticsProfitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.logisticsprofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsProfitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
