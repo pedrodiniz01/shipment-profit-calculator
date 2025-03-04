375b3b9edac25e0175fd10e5bbcc5f0349b82620
pedrodiniz01 <1170053@isep.ipp.pt>
2025-03-04 23:38:27 +0000

    delete unecesasry const

--- backend/src/main/java/com/company/shipmentsprofit/constants/Constants.java
+++ backend/src/main/java/com/company/shipmentsprofit/constants/Constants.java
@@ -3,7 +3,6 @@ package com.company.shipmentsprofit.constants;
 public class Constants {
 
     public class Exceptions {
-        public static final String REFERENCE_NUMBER_NOT_FOUND = "Reference number not found.";
         public static final String INVALID_INPUT_EXCEPTION = "Invalid input data.";
         public static final String DUPLICATED_REFERENCE_NUMBER_EXCEPTION = "Duplicated reference number.";
         public static final String TRANSACTION_AMOUNT_NOT_VALID = "Transaction amount not valid.";
